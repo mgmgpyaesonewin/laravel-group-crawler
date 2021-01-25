3e1831d1e9b93c3feba8d079d306f26eef7cdb27
Pyae Sone <mgmgpyaesonewin@gmail.com>
2021-01-25 21:20:38 +0630

    updated

--- database/migrations/2021_01_24_180755_create_posts_table.php
+++ database/migrations/2021_01_24_180755_create_posts_table.php
@@ -15,7 +15,6 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string('url');
             $table->string('profile_name');
             $table->string('profile_link');
             $table->string('link');
